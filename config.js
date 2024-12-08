df360f9b9b37196a1af235e2654853da28183d42
Lisafax <lisafax93@gmail.com>
2024-12-08 23:15:50 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  ownerUsernames: ["MOHAMMADNAYAN"],//Your Username
-  ownerNumber: "+8801615298449",//Your Number
-  admin: ["5920596290"], // your id
+  ownerUsernames: ["nisan_editz198"],//Your Username
+  ownerNumber: "+88017××××××98",//Your Number
+  admin: ["5443875693"], // your id
   prefix: "/", // dont change prefix 
-  telegramBotToken: "Your_Bot_Token",//Your Bot Tokens
-  imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
+  telegramBotToken: "7847188789:AAG64QkyWM7V8lvbkXonztlD_X5RNNAGaAo",//Your Bot Tokens
+  imageUrl: "https://i.imgur.com/WRDSYlU.gif",//Thumbnail Url
   port: process.env.PORT || 8053 || 8053, // port
   mess: {
-    ownerlink: "https://t.me/MOHAMMADNAYAN",
+    ownerlink: "https://t.me/@nisan_editz198",
     eror: "Internal Server Eror 😵",
     owner: "Sorry, this command can only be accessed by the owner!",
     group: "Sorry, this command can only be used within a group!",
